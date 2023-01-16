0730c2121778b3826ed33e090ae4964f292e2f4b
Brad Kanther <Kapanther@golderbne.onmicrosoft.com>
2023-01-17 02:28:13 +1000

    some debugging

--- media/lua/client/Exterminator.lua
+++ media/lua/client/Exterminator.lua
@@ -15,6 +15,8 @@ local text_count = ""
 local text_density = ""
 local text_spawned = ""
 local floor = math.floor
+local isScannerOnMK1 = false
+local eventUI = ""
 
 local function getCellData(playerX,PlayerY)	
 	--Cell Data
@@ -42,23 +44,45 @@ end
 local function drawZombieCount()
 	--Check if they are holding a zombie scanner
 	
-	if player then
-		local isHoldingZombieScanner = player:getInventory():contains("Exterminator.ZombieScannerMK1"); --todo zombie scanner needs to be detected in hand
-		if isHoldingZombieScanner then
-			local playerX = floor(player:getX()); --because getX returns float
-			local playerY = floor(player:getY());
+	if player then		
+		local playerX = floor(player:getX()); --because getX returns float
+		local playerY = floor(player:getY());
 
-			getCellData(playerX,playerY)
-			text_count =  "Zcount = " .. cache_celldata
-			text_density =  "Density = " .. cache_gridZombieDensity
-			text_spawned =  "Spawned = " .. cache_gridZombieSpawned
-			textManager:DrawString(UIFont.Large, screenX, screenY, text_count, 0.1, 0.8, 1, 1);
-			textManager:DrawString(UIFont.Large, screenX, screenY + 30, text_density, 0.1, 0.8, 1, 1);
-			textManager:DrawString(UIFont.Large, screenX, screenY + 60, text_spawned, 0.1, 0.8, 1, 1);
-			textManager:DrawString(UIFont.Large, screenX, screenY + 90, tostring(isHoldingZombieScanner), 0.1, 0.8, 1, 1);
-		end
+		getCellData(playerX,playerY)
+		text_count =  "Zcount = " .. cache_celldata
+		text_density =  "Density = " .. cache_gridZombieDensity
+		text_spawned =  "Spawned = " .. cache_gridZombieSpawned
+		textManager:DrawString(UIFont.Large, screenX, screenY, text_count, 0.1, 0.8, 1, 1);
+		textManager:DrawString(UIFont.Large, screenX, screenY + 30, text_density, 0.1, 0.8, 1, 1);
+		textManager:DrawString(UIFont.Large, screenX, screenY + 60, text_spawned, 0.1, 0.8, 1, 1);
+		textManager:DrawString(UIFont.Large, screenX, screenY + 90, tostring(isScannerOnMK1), 0.1, 0.8, 1, 1);
 	end
 end
+
+local function checkZombieScannerMK1(player_)
+
+	if player_ then
+		local primaryItem = player_:GetPrimaryEquippedCache():getName();
+		local secondaryItem = player_:GetSecondaryEquippedCache():getName();
+		if primaryItem == "ZombieScannerMK1" then
+			return true
+		elseif secondaryItem == "ZombieScannerMK1" then
+			return true
+		else
+			return false
+		end
+	else
+	return false
+	end
+end
+
+Events.OnEquipPrimary.Add(function()
+	isScannerOnMK1 = checkZombieScannerMK1(player)
+end)
+
+Events.OnEquipSecondary.Add(function()
+	isScannerOnMK1 = checkZombieScannerMK1(player)
+end)
 
 Events.OnGameStart.Add(function()
 	Events.OnPostUIDraw.Add(drawZombieCount);
