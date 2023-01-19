a5e756f8a8df3fe83eb80a45fa21bc4fd0dc596e
Brad Kanther <Kapanther@golderbne.onmicrosoft.com>
2023-01-20 00:04:25 +1000

    strugglign with events ui now showing up anymore

--- media/lua/client/Exterminator.lua
+++ media/lua/client/Exterminator.lua
@@ -1,10 +1,33 @@
-if EXM then
-	return print("ERROR: Can't enable Exterminator MOD because it is already enabled!")
-end
-EXM = {}
-local EXM_DATA = EXM
+---
+--- Mod: Exterminator
+--- Workshop: <Add Address
+--- Author: Kuckpanther
+--- Profile: <AddProfile
+---
+--- Redistribution of this mod without explicit permission from the original creator is prohibited
+--- under any circumstances. This includes, but not limited to, uploading this mod to the Steam Workshop
+--- or any other site, distribution as part of another mod or modpack, distribution of modified versions.
+--- You are free to do whatever you want with the mod provided you do not upload any part of it anywhere.
+---
 
+Exterminator = Exterminator or {}
+
+local isHoldingScanner = false
 local textManager = getTextManager();
+
+function Exterminator.Initialize()
+
+	--add zombie scanner check to equip Primary or secondary
+	Events.OnEquipPrimary.Add(Exterminator.checkZombieScannerEquipped)
+	Events.OnEquipSecondary.Add(Exterminator.checkZombieScannerEquipped)
+
+	--check if we are currently holding a zombie scanner	
+	if getPlayer() then
+		player = getPlayer()
+		Exterminator.checkZombieScannerEquipped()
+	end
+end
+
 local player = nil
 local screenX = 65;
 local screenY = 13;
@@ -15,13 +38,12 @@ local text_count = ""
 local text_density = ""
 local text_spawned = ""
 local floor = math.floor
-local isScannerOnMK1 = false
-local eventUI = ""
+local primaryItem = ""
+local secondaryItem = ""
 
-local function getCellData(playerX,PlayerY)	
+function getCellData(playerX,PlayerY)	
 	--Cell Data
-	local celldata = getWorld():getCell():getZombieList():size();
-	EXM_DATA.celldata = celldata	
+	local celldata = getWorld():getCell():getZombieList():size();		
 	if celldata then
 		cache_celldata = celldata
 		else
@@ -41,13 +63,11 @@ local function getCellData(playerX,PlayerY)
 	end
 end
 
-local function drawZombieCount()
+function Exterminator.drawZombieCount()
 	--Check if they are holding a zombie scanner
-	
 	if player then		
 		local playerX = floor(player:getX()); --because getX returns float
 		local playerY = floor(player:getY());
-
 		getCellData(playerX,playerY)
 		text_count =  "Zcount = " .. cache_celldata
 		text_density =  "Density = " .. cache_gridZombieDensity
@@ -55,42 +75,35 @@ local function drawZombieCount()
 		textManager:DrawString(UIFont.Large, screenX, screenY, text_count, 0.1, 0.8, 1, 1);
 		textManager:DrawString(UIFont.Large, screenX, screenY + 30, text_density, 0.1, 0.8, 1, 1);
 		textManager:DrawString(UIFont.Large, screenX, screenY + 60, text_spawned, 0.1, 0.8, 1, 1);
-		textManager:DrawString(UIFont.Large, screenX, screenY + 90, tostring(isScannerOnMK1), 0.1, 0.8, 1, 1);
 	end
 end
 
-local function checkZombieScannerMK1(player_)
+function Exterminator.onRenderTick()
+	Events.OnRenderTick.Add(function() 
+	Events.OnPostUIDraw.Add(Exterminator.drawZombieCount);
+	end)
+end
 
-	if player_ then
-		local primaryItem = player_:GetPrimaryEquippedCache():getName();
-		local secondaryItem = player_:GetSecondaryEquippedCache():getName();
-		if primaryItem == "ZombieScannerMK1" then
-			return true
-		elseif secondaryItem == "ZombieScannerMK1" then
-			return true
-		else
-			return false
-		end
-	else
-	return false
+function Exterminator.checkZombieScannerEquipped()
+	if player then
+		primaryItem = player:getPrimaryHandItem()
+		secondaryItem = player:getSecondaryHandItem()
+		--if primaryItem == "ZombieScannerMK1" then		
+		    --TODO fix this so it is a function that can be added and removed
+			Exterminator.onRenderTick()
+			--return true
+		--elseif secondaryItem == "ZombieScannerMK1" then
+			--Events.OnPostUIDraw.Add(drawZombieCount); --TODO fix this so it is a function that can be added and removed
+		--elseaaaaaaaa
+			--return false
+		--end
 	end
 end
 
-Events.OnEquipPrimary.Add(function()
-	isScannerOnMK1 = checkZombieScannerMK1(player)
-end)
+Events.OnGameStart.Add(Exterminator.Initialize)
 
-Events.OnEquipSecondary.Add(function()
-	isScannerOnMK1 = checkZombieScannerMK1(player)
-end)
 
-Events.OnGameStart.Add(function()
-	Events.OnPostUIDraw.Add(drawZombieCount);
-end)
 
-Events.OnCreatePlayer.Add(function()
-	player = getPlayer()	
-end)
 
 
 
