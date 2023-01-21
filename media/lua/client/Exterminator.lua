8cfe44fff15f81cda219480f43045cfba269c880
Brad Kanther <Kapanther@golderbne.onmicrosoft.com>
2023-01-22 00:41:10 +1000

    amended equip primary code

--- media/lua/client/Exterminator.lua
+++ media/lua/client/Exterminator.lua
@@ -12,42 +12,40 @@
 
 Exterminator = Exterminator or {}
 
-local isHoldingScanner = false
 local textManager = getTextManager();
 
 function Exterminator.Initialize()
-
+	print("EXM:Initialize")
 	--add zombie scanner check to equip Primary or secondary
-	Events.OnEquipPrimary.Add(Exterminator.checkZombieScannerEquipped)
-	Events.OnEquipSecondary.Add(Exterminator.checkZombieScannerEquipped)
+	Events.OnEquipPrimary.Add(Exterminator.OnEquipPrimary)
+	--Events.OnEquipSecondary.Add(OnEquipPrimary)	
 
 	--check if we are currently holding a zombie scanner	
-	if getPlayer() then
-		player = getPlayer()
-		Exterminator.checkZombieScannerEquipped()
+	if getPlayer() and getPlayer():getPrimaryHandItem() then		
+		Exterminator.OnEquipPrimary(getPlayer())
 	end
 end
 
-local player = nil
 local screenX = 65;
 local screenY = 13;
-local cache_celldata = 0
-local cache_gridZombieDensity = 0
-local cache_gridZombieSpawned = 0
-local text_count = ""
-local text_density = ""
-local text_spawned = ""
-local floor = math.floor
-local primaryItem = ""
-local secondaryItem = ""
+local cache_zombieCount = 0;
+local cache_gridZombieDensity = 0;
+local cache_nearestZombieDistance = 0;
+local text_count = "";
+local text_density = "";
+local text_spawned = "";
+local floor = math.floor;
+local primaryItem = "";
+local secondaryItem = "";
 
-function getCellData(playerX,PlayerY)	
+function Exterminator.getZombieScanData(playerX,PlayerY)
+	--print("EXM:getCellData")
 	--Cell Data
-	local celldata = getWorld():getCell():getZombieList():size();		
-	if celldata then
-		cache_celldata = celldata
+	local zombieCount = getWorld():getCell():getZombieList():size();		
+	if zombieCount then
+		cache_zombieCount = zombieCount
 		else
-		cache_celldata = 0
+		cache_zombieCount = 0
 	end
 	local gridZombieDensity = getWorld():getMetaGrid():getZoneAt(playerX,PlayerY,0):getZombieDensity();
 	if gridZombieDensity then
@@ -55,49 +53,45 @@ function getCellData(playerX,PlayerY)
 		else
 		cache_gridZombieDensity = 99999
 	end
-	local gridZombiesSpawned = getWorld():getMetaGrid():getZoneAt(playerX,PlayerY,0).spawnedZombies
-	if gridZombiesSpawned then
-		cache_gridZombieSpawned = 11111 -- TODO get the acutal spawned from the hashmap
-		else
-		cache_gridZombieSpawned = 99999
+	if zombieCount > 0 then
+		local zombieList = getPlayer();
+		--for zombies = 1,zombieCount do
+			--zombie = zombieList:get(zombies);
+			print("ClosestZombie")
+		--end
+		cache_nearestZombieDistance = 9999
 	end
 end
 
 function Exterminator.drawZombieCount()
+	--print("EXM:drawZombieCount")
+	local player = getPlayer()
 	--Check if they are holding a zombie scanner
-	if player then		
+	if player then
+		--print("EXM:drawZombieCount:playerFound")
 		local playerX = floor(player:getX()); --because getX returns float
 		local playerY = floor(player:getY());
-		getCellData(playerX,playerY)
-		text_count =  "Zcount = " .. cache_celldata
-		text_density =  "Density = " .. cache_gridZombieDensity
-		text_spawned =  "Spawned = " .. cache_gridZombieSpawned
-		textManager:DrawString(UIFont.Large, screenX, screenY, text_count, 0.1, 0.8, 1, 1);
-		textManager:DrawString(UIFont.Large, screenX, screenY + 30, text_density, 0.1, 0.8, 1, 1);
-		textManager:DrawString(UIFont.Large, screenX, screenY + 60, text_spawned, 0.1, 0.8, 1, 1);
+		Exterminator.getZombieScanData(playerX,playerY)
+		text_zombieCount =  "Zcount = " .. cache_zombieCount
+		text_nearestZombie =  "Nearest Zombie = " .. cache_nearestZombieDistance
+		textManager:DrawString(UIFont.Large, screenX, screenY, text_zombieCount, 0.1, 0.8, 1, 1);
+		textManager:DrawString(UIFont.Large, screenX, screenY + 30, text_nearestZombie, 0.1, 0.8, 1, 1);
 	end
 end
 
-function Exterminator.onRenderTick()
-	Events.OnRenderTick.Add(function() 
-	Events.OnPostUIDraw.Add(Exterminator.drawZombieCount);
-	end)
+function Exterminator.OnEquipPrimary(player)
+	--print("EXM:OnEquipPrimary")
+	if not player:isLocalPlayer() then return end
+		if Exterminator.isZombieScanner(getPlayer():getPrimaryHandItem()) then
+			Events.OnPostUIDraw.Add(Exterminator.drawZombieCount)
+		else
+			Events.OnPostUIDraw.Remove(Exterminator.drawZombieCount)
+		end	
 end
 
-function Exterminator.checkZombieScannerEquipped()
-	if player then
-		primaryItem = player:getPrimaryHandItem()
-		secondaryItem = player:getSecondaryHandItem()
-		--if primaryItem == "ZombieScannerMK1" then		
-		    --TODO fix this so it is a function that can be added and removed
-			Exterminator.onRenderTick()
-			--return true
-		--elseif secondaryItem == "ZombieScannerMK1" then
-			--Events.OnPostUIDraw.Add(drawZombieCount); --TODO fix this so it is a function that can be added and removed
-		--elseaaaaaaaa
-			--return false
-		--end
-	end
+function Exterminator.isZombieScanner(item)
+	print("EXM:isZombieScanner")
+    return instanceof(item, "DrainableComboItem")-- and item:getType()
 end
 
 Events.OnGameStart.Add(Exterminator.Initialize)
---
+++ media/lua/client/ExterminatorConfig.lua
@@ -0,0 +1,10 @@
+Exterminator = Exterminator or {}
+Exterminator.Config = Exterminator.Config or {}
+
+Exterminator.Config.BLINK_COUNT_MAX = 3
+Exterminator.Config.BLINK_ALPHA_MAX = 0.5
+
+Exterminator.Config.COLOR_GREEN = { r = 0, g = 0.6, b = 0 }
+Exterminator.Config.COLOR_YELLOW = { r = 0.9, g = 0.9, b = 0 }
+Exterminator.Config.COLOR_RED = { r = 1, g = 0, b = 0 }
+Exterminator.Config.COLOR_BLUE = { r = 0, g = 0, b = 1 }
